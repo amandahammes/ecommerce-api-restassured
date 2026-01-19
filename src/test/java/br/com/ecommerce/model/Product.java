b57764f5773b500a8db3e74f2c0270ca107fc454
996911 <amanda.kopper.external@lojasrenner.com.br>
2026-01-19 17:08:28 -0300

    chore: criaÃcao de pastas e estrutura para testes product

---
+++ src/test/java/br/com/ecommerce/model/Product.java
@@ -0,0 +1,73 @@
+package br.com.ecommerce.model;
+
+public class Product {
+    private Integer sku;
+    private String name;
+    private Integer categoryId;
+    private Integer priceCents;
+    private String currency;
+    private boolean active;
+    private Integer stockQuantity;
+
+    public Product(Integer sku, String name, Integer categoryId, Integer priceCents, String currency, boolean active, Integer stockQuantity) {
+        this.sku = sku;
+        this.name = name;
+        this.categoryId = categoryId;
+        this.priceCents = priceCents;
+        this.currency = currency;
+        this.active = active;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Integer getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getPriceCents() {
+        return priceCents;
+    }
+
+    public void setPriceCents(Integer priceCents) {
+        this.priceCents = priceCents;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
---
+++ src/test/java/br/com/ecommerce/test/ProductTest.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.test;
+
+import br.com.ecommerce.util.ConfigLoader;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class ProductTest {
+    @BeforeAll
+    public static void setup(){
+        RestAssured.baseURI = ConfigLoader.getProperty("base_url");
+    }
+
+}
